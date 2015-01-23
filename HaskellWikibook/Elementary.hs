a7e396c9a6c010681e46398b5bd0802eb46727f6
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-01-23 22:24:49 +0100

    fact

---
+++ HaskellWikibook/Elementary.hs
@@ -0,0 +1,9 @@
+fact1 n = 
+    if n < 0 then error "cannot be < 0"
+    else if n == 0 then 1
+    else n * fact1 (n-1)
+
+fact2 0 = 1
+fact2 n = 
+    if n < 0 then error "cannot be < 0"
+    else n * fact2 (n - 1)
