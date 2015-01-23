14a058ed9193167c5a9bccfdabb47b2d02dad42f
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-01-23 22:59:04 +0100

    power ok

--- HaskellWikibook/Elementary.hs
+++ HaskellWikibook/Elementary.hs
@@ -1,14 +1,3 @@
-factByAcc n = acc n 1
-    where
-    acc cnt res
-        | cnt <= 1   = res
-        | otherwise  = acc (cnt - 1) (cnt * res)
-
-factByAcc2 n = acc n 1 where
-    acc cnt res
-        | cnt <= 1   = res
-        | otherwise  = acc (cnt - 1) (cnt * res)        
-
 fact1 n = 
     if n < 0 then error "cannot be < 0"
     else if n == 0 then 1
@@ -30,3 +19,24 @@ doublefact2 n
     | n == 0 = 1
     | n == 1 = 1
     | otherwise = n * doublefact2 (n - 2)
+
+-- VV traduction iteration + mutation ->recurs
+factByAcc n = acc n 1
+    where
+    acc cnt res
+        | cnt <= 1   = res
+        | otherwise  = acc (cnt - 1) (cnt * res)
+
+factByAcc2 n = acc n 1 where
+    acc cnt res
+        | cnt <= 1   = res
+        | otherwise  = acc (cnt - 1) (cnt * res)        
+
+mult _ 0 = 0         
+mult x 1 = x
+mult x y = x + (mult x (y - 1))
+
+--power x n
+power x 0 = 1
+power x 1 = x
+power x n = x * power x (n - 1)
