23513f1d4a793ae7a36b721b078a856bfe4dd3ff
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-01-23 22:32:28 +0100

    +doublefact

--- HaskellWikibook/Elementary.hs
+++ HaskellWikibook/Elementary.hs
@@ -7,3 +7,15 @@ fact2 0 = 1
 fact2 n = 
     if n < 0 then error "cannot be < 0"
     else n * fact2 (n - 1)
+
+doublefact 0 = 1
+doublefact 1 = 1
+doublefact n =
+    if n < 0 then error "cannot be < 0"
+    else n * doublefact (n - 2)
+
+doublefact2 n
+    | n < 0 = error "cannot be < 0"
+    | n == 0 = 1
+    | n == 1 = 1
+    | otherwise = n * doublefact2 (n - 2)
