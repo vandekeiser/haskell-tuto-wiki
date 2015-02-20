aebd22bc548c9d7bd29d963f1f3b2cbfb8899866
Laurent CLAISSE <laurent.claisse@zenika.com>
2015-02-20 10:15:50 +0100

    Intermediary.doGuessing

---
+++ HaskellWikibook/Intermediary.hs
@@ -0,0 +1,21 @@
+module Intermediary where
+
+doGuessing num = do
+  putStrLn "Enter your guess:"
+  guess <- getLine
+  case compare (read guess) num of
+    LT -> do putStrLn "Too low!"
+             doGuessing num
+    GT -> do putStrLn "Too high!"
+             doGuessing num
+    EQ -> putStrLn "You Win!"
+
+doGuessingBraces num = do {
+    putStrLn "Enter your guess:"; guess <- getLine;
+    case compare (read guess) num of {
+        LT -> do { putStrLn "Too low!" ;  doGuessing num; };
+        GT -> do { putStrLn "Too high!"; doGuessing num; };
+        EQ -> putStrLn "You Win!";
+    }
+}
+  
